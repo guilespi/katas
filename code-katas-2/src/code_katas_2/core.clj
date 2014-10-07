87520df46e8ed648cbcb69a2af7e7da0f9895d35
Guillermo Winkler <guillermo.winkler@gmail.com>
2014-10-07 10:41:44 -0200

    soluciones practico 2

--- code-katas-2/src/code_katas_2/core.clj
+++ code-katas-2/src/code_katas_2/core.clj
@@ -5,22 +5,41 @@
   "Escribir una funcion que acepte una funcion parcial con cantidad de argumentos desconocida,
    retornar una funcion equivalente de n argumentos"
   [f]
-  )
+  (fn [& args]
+    (let [r (f (first args))]
+      (if (fn? r)
+        (apply (unpartial r) (rest args))
+        r))))
 
 
 (defn search
   "Dado un numero cualquiera de secuencias, cada una ya ordenada de menor a mayor, encontrar el numero
    mas chico que aparezca en todas las secuencias, las secuencias pueden ser infinitas."
   [& seqs]
-  )
+  (letfn [(lazy-search [xs]
+            (let [s (map first xs)
+                  m (apply min s)]
+              (if (apply = s)
+                m
+                (lazy-search (map #(if (= (first %) m) (rest %) %) xs)))))]
+    (lazy-search seqs)))
 
 
 (defn intercalar
   "Escriba una funcion que tome un predicado de 2 argumentos, un valor y una coleccion, y
    retorne una nueva coleccion donde el valor es insertado intercalado cada dos argumentos
    que cumplan el predicado"
-  [predicado valor secuencia]
-  )
+  ;;  [predicado valor secuencia]
+  [pred value col]
+  (letfn [(looper [s head]
+            (lazy-seq
+             (if-let [sec (first s)]
+               (if (pred head sec)
+                 (cons head (cons value (looper (rest s) sec)))
+                 (cons head (looper (rest s) sec)))
+               (if head [head] []))))]
+    (looper (rest col) (first col))))
+
 
 
 (defn tartamudeo
@@ -29,5 +48,8 @@
 
    La funcion debe aceptar una secuencia inicial de numeros, y devolver una secuencia infinita de compresiones, donde
    cada nuevo elemento es el elemento anterior comprimido."
-  [secuencia]
-  )
+  ;[secuencia]
+  [s]
+  (letfn [(pronuntiate [l]
+            (apply concat (map (juxt count first) (partition-by identity l))))]
+    (iterate pronuntiate (pronuntiate s))))
